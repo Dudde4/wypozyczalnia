5f0229421ebd44046acb431c879932c9600202b2
Szymon <lapniewskiszymon7@gmail.com>
2025-04-06 18:22:06 +0200

    rozwijana lista godzin

--- app/src/main/java/com/example/wypozyczalnia/SearchActivity.java
+++ app/src/main/java/com/example/wypozyczalnia/SearchActivity.java
@@ -1,7 +1,13 @@
 package com.example.wypozyczalnia;
 
 import android.os.Bundle;
-
+import android.widget.Spinner;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.List;
+import java.util.ArrayList;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -16,6 +22,21 @@ public class SearchActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.search_layout);
 
+        Spinner spinner2 = findViewById(R.id.spinner2);
+        Spinner spinner3 = findViewById(R.id.spinner3);
+
+        List<String> hoursList = new ArrayList<>();
+        for (int hour = 8; hour <= 16; hour++) {
+            String hourString = String.format("%02d:00", hour);
+            hoursList.add(hourString);
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, hoursList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner2.setAdapter(adapter);
+        spinner3.setAdapter(adapter);
 
     }
 }
